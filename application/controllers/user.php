9e58387cec343f21c81bd294c3fb3cd510c09b16
Andikariski <andikariski207@gmail.com>
2020-12-21 16:48:32 +0700

    Update tampilan public user jadi dinamis

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -3,27 +3,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-
+    public function __construct()
+    {
+        parent::__construct();
+        // is_logged_in();
+        $this->load->model('Admin');
+    }
 
     public function index()
     {
-        $this->load->view('templateFE/navbar.php');
-        $this->load->view('public/index');
-        $this->load->view('templateFE/footer.php');
-    }
 
-    public function kasusCovid()
-    {
+        $data['tbl_pencegahan'] = $this->Admin->getAlldataPencegahan();
+        $data['tbl_gejala'] = $this->Admin->getAlldatagejala();
         $this->load->view('templateFE/navbar.php');
-        $this->load->view('public/kasus');
-        $this->load->view('templateFE/footer.php');
-    }
-
-
-    public function rumahSakit()
-    {
-        $this->load->view('templateFE/navbar.php');
-        $this->load->view('public/rumahsakit');
+        $this->load->view('public/index', $data);
         $this->load->view('templateFE/footer.php');
     }
 }
--- application/views/public/index.php
+++ application/views/public/index.php
@@ -22,72 +22,22 @@
         <p class="text-secondary text-center w-75 mx-auto">Jangan Panik, Kenali gejalanya dan deteksi COVID-19 sejak dini. Hanya perlu jaga jarak, hindari jabat tangan dan bila sakit memakai masker, dan jaga kebersihan</p>
         <!-- card gejala -->
         <div class="row mt-5">
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/demam.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Demam</h5>
-                        <p class="card-text">Demam menunjukkan gejala awal COVID-19, demam biasanya mencapai suhu 38 derajat celcius keatas.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/batuk.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Batuk Kering</h5>
-                        <p class="card-text">Sering Batuk Berulang-ulang merupakan tanda infeksi di paru-paru, yang mungkin disebabkan COVID-19.</p>
+
+            <?php foreach ($tbl_gejala as $g) : ?>
+                <div class="col-md-5 col-lg-4">
+                    <div class="card service-card">
+                        <div class="service-card__img">
+                            <img src="./assets/img_gejala/<?= $g['gambar'] ?>" alt="gejala_img">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $g['gejala'] ?></h5>
+                            <p class="card-text"><?= $g['deskripsi'] ?></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/sakittenggorokan.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Sakit Tenggorokan</h5>
-                        <p class="card-text">Infeksi COVID-19 akan melalui jalur pernafasan sehingga mengakibatkan radang tenggorokan dan rasa nyeri.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/sakitkepala.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Sakit Kepala</h5>
-                        <p class="card-text">Dengan adanya demam yang tinggi, COVID-19 seringkali diikuti dengan gejala sakit kepala.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/lemas.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Lemas</h5>
-                        <p class="card-text">Infeksi COVID-19 yang telah menyebar ke seluruh tubuh akan menghabiskan energi yang banyak sehingga dapat mengakibatkan lemas.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card service-card">
-                    <div class="service-card__img">
-                        <img src="./assets/assetsimg/gejala_img/sesak.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Sesak Nafas</h5>
-                        <p class="card-text">Apabila COVID-19 berkembang dengan cepat dan besar di paru-paru, fungsi paru akan menurun sehingga terjadi sesak nafas.</p>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
+
+
         </div>
         <!-- card gejala ends -->
     </section>
@@ -100,72 +50,20 @@
         <p class="text-secondary text-center w-75 mx-auto">Lindungi diri dan orang lain di sekitar Anda dengan mengetahui fakta-fakta terkait virus ini dan mengambil langkah pencegahan yang sesuai. Ikuti saran yang diberikan oleh badan kesehatan publik lokal Anda</p>
         <!-- card and image pencegahan section -->
         <div class="row justify-content-center mt-5">
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/cuci_tangan.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Cuci Tangan</h5>
-                        <p class="card-text">Cuci tangan sebelum dan sesudah melakukan aktifitas.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/sabun.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Gunakan Sabun Antiseptic</h5>
-                        <p class="card-text">Sabun Antiseptic bertujuan untuk membunuh kuman yang menempel pada tangan</p>
+
+            <?php foreach ($tbl_pencegahan as $p) : ?>
+                <div class="col-md-5 col-lg-4">
+                    <div class="card pencegahan-covid-card">
+                        <div class="pencegahan-covid-card__img">
+                            <img src="./assets/img_pencegahan/<?= $p['gambar'] ?>" alt="gejala_img">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $p['cara_pencegahan'] ?></h5>
+                            <p class="card-text"><?= $p['deskripsi'] ?></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/masker.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Selalu Gunakan Masker</h5>
-                        <p class="card-text">Gunakan masker untuk mencegah ataupun mengurangi droplet</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/kerumunan.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Hindari Kerumunan</h5>
-                        <p class="card-text">Tujuannya untuk memutus penyebaran COVID-19 di kerumunan</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/jabattangan.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Hindari Kontak Langsung</h5>
-                        <p class="card-text">Hindari jabat tangan dengan orang lain.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 col-lg-4">
-                <div class="card pencegahan-covid-card">
-                    <div class="pencegahan-covid-card__img">
-                        <img src="./assets/assetsimg/pencegahan_img/desin.png" alt="gejala_img">
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Semprotkan Desinfektan</h5>
-                        <p class="card-text">Bersihkan benda yang sering disentuh agar selalu steril dengan desinfektan.</p>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
             <!-- <div class="col-md-6 col-lg-5">
                     <div class="jumbotron__img">
                         <img src="./assets/pencegahan_img/pencegahan.png" class="img-fluid" alt="" />
